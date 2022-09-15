5377ebf41875503638bd3f7cd8a9e89d7d9de3c3
Kateryna Novikova <0802novikova@gmail.com>
2022-09-15 14:50:17 +0300

    deleted getters and setters

--- src/Employee.java
+++ src/Employee.java
@@ -16,37 +16,6 @@ public class Employee {
         this.id = nextId++;
     }
 
-    public void setName(String name) throws FieldLengthLimitException {
-        this.checkLength(name);
-        this.name = name;
-    }
-
-    public void setSurname(String surname) throws FieldLengthLimitException {
-        this.checkLength(surname);
-        this.surname = surname;
-    }
-
-    public void setSalary(double salary) throws IncorrectSalaryException {
-        this.checkSalary(salary);
-        this.salary = salary;
-    }
-
-    public int getId() {
-        return this.id;
-    }
-
-    public String getName() {
-        return this.name;
-    }
-
-    public String getSurname() {
-        return this.surname;
-    }
-
-    public double getSalary() {
-        return this.salary;
-    }
-
     private void checkLength(String namePart) throws FieldLengthLimitException {
         if (namePart != null && namePart.length() > MAX_CHARACTERS) {
             throw new FieldLengthLimitException("Too many characters in \"" + namePart +  "\". Length of name/surname field must be <" + MAX_CHARACTERS);
