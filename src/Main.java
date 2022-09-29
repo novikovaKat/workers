8375299401e77978ec8782f9be7c2ff5e93a1c03
Kateryna Novikova <0802novikova@gmail.com>
2022-09-29 15:02:20 +0300

    correct main

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,9 @@ public class Main {
     public static void main(String[] args){
         Registry registry = Registry.getInstance();
         try {
-            Employee emp1 = new Employee("Diana", "Koval", "Kyiv", 30000,1 );
-            Employee emp2 = new Employee("Sasha", "Brown", "Lviv", 25900, 2);
-            Employee emp3 = new Manager("Denis", "Korchynskyi", "Lviv", 32000, 1, 234);
+            Employee emp1 = new Employee("Diana", "Koval", "Kyiv", 30000,3 );
+            Employee emp2 = new Employee("Sasha", "Brown", "Lviv", 25900, 3);
+            Employee emp3 = new Manager("Denis", "Korchynskyi", "Lviv", 32000, 3, 234);
 
             registry.addWorker(emp1);
             registry.addWorker(emp2);
